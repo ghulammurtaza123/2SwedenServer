381086e9a89ad1cfa999e39f319de062d71cbdbd
ghuli <ghulammurtaza123@gmail.com>
2019-06-14 12:58:47 +0200

    enabled cors

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = express()
 const port = process.env.PORT || 5000
 const apiRoutes = require('./routes/api')
 const apiRoutes2 = require('./routes/api2')
+const cors= require('cors')
 
 app.listen(port, () => console.log("Server listening on port", port))
-
+app.use(cors())
 app.use("/api",apiRoutes)
 app.use("/api2",apiRoutes2)
 
